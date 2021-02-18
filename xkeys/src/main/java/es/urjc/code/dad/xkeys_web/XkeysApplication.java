97907bc8044f123574ced147ffa68c590b4dee0a
alv4rob <alvarobarrio99@gmail.com>
2021-02-18 16:59:30 +0100

    Proyecto de prueba

---
+++ xkeys/src/main/java/es/urjc/code/dad/xkeys_web/XkeysApplication.java
@@ -0,0 +1,13 @@
+ package es.urjc.code.dad.xkeys_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XkeysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XkeysApplication.class, args);
+	}
+
+}
---
+++ xkeys/src/test/java/es/urjc/code/dad/xkeys_web/XkeysApplicationTests.java
@@ -0,0 +1,13 @@
+package es.urjc.code.dad.xkeys_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XkeysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
