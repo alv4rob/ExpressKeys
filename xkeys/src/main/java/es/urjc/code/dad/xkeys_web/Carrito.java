5d8131d898336ee58f869b4e72a94a60d6128b67
alv4rob <alvarobarrio99@gmail.com>
2021-03-03 20:17:18 +0100

    Sesiones de usuario, carrito y ver stock

---
+++ xkeys/src/main/java/es/urjc/code/dad/xkeys_web/Carrito.java
@@ -0,0 +1,59 @@
+package es.urjc.code.dad.xkeys_web;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class Carrito {
+	
+	private ArrayList<Producto> carrito;
+	private int precioTotal;
+	
+	public Carrito() {
+		
+		carrito = new ArrayList<>();
+		precioTotal = 0;
+		
+	}
+	
+	public void añadirAlCarrito(Producto producto) {
+		carrito.add(producto);	
+		precioTotal = precioTotal + producto.getPrecio();
+		
+	}
+	
+	public void quitarDelCarrito(Producto producto) {
+	    carrito.remove(producto);
+	    precioTotal = precioTotal - producto.getPrecio();
+	  
+	}
+	
+	public int nCarrito( ) {
+	    return carrito.size();
+	}
+
+	public int getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void setPrecioTotal(int precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+
+	public ArrayList<Producto> getCarrito() {
+		return carrito;
+	}
+
+	public void setCarrito(ArrayList<Producto> carrito) {
+		this.carrito = carrito;
+	}
+
+	public boolean esVacio() {
+		
+		return carrito.size()==0;
+	}
+
+}
